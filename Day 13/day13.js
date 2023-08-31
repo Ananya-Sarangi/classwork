216a2afeabb1ad60c325a3e6a881500e8762875a
Ananya-Sarangi <141609971+Ananya-Sarangi@users.noreply.github.com>
2023-08-31 14:28:41 +0530

    Add files via upload

---
+++ Day 13/day13.js
@@ -0,0 +1,29 @@
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      dynamicBullets: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+
+    autoplay: {
+        delay: 3000, // Set the delay between slides in milliseconds
+        disableOnInteraction: false, // Allow manual interaction (like clicking) to not stop the autoplay
+    },
+
+  });
